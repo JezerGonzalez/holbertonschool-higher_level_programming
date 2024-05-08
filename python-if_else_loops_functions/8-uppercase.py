3fc18e1b41df065d25f1cb49cdd19d0037a035fe
JezerGonzalez <jezergonzalez@gmail.com>
2024-05-08 15:04:50 -0400

    added

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for idx in str:
+        if 'a' <= idx <= 'z':
+            upper = chr(ord(idx) - 32)
+        else:
+            upper = idx
+        print("{}".format(upper), end="")
+    print()
