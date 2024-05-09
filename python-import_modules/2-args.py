5601bab12a28be0b3870e5d28d55b890fc341247
JezerGonzalez <jezergonzalez@gmail.com>
2024-05-09 14:12:05 -0400

    fu

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    if argv == 1:
+    if len(argv) == 1:
         print("0 arguments.")
-    elif argv == 2:
+    elif len(argv) == 2:
         print("1 argument:")
         print("1 {}".format(argv[1]))
     else:
