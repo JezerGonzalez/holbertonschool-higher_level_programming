fcca57795cc10a5c27997e21e3d812e68c04f247
JezerGonzalez <jezergonzalez@gmail.com>
2024-05-10 13:18:05 -0400

    fixed

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-from sys import argv
+import sys
 from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     
-    a = int(argv[1])
-    b = int(argv[3])
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
     operations = {"+": add, "-": sub, "*": mul, "/": div}
 
-    if len(argv) - 1 != 3:
+    if len(sys.argv) - 1 != 3:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-    if argv[2] not in list(operations.keys()):
+        sys.exit(1)
+    if sys.argv[2] not in list(operations.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-    result = operations[argv[2]](a, b)
-    print("{} {} {} = {}".format(a, argv[2], b, result))
+        sys.exit(1)
+    result = operations[sys.argv[2]](a, b)
+    print("{} {} {} = {}".format(a, sys.argv[2], b, result))
