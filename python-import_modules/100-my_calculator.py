40b0dae9fc01e6fd3c22a8da724ec2d4c29837a4
JezerGonzalez <jezergonzalez@gmail.com>
2024-05-10 14:09:08 -0400

    fixed??

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,10 +5,10 @@ if __name__ == "__main__":
     operations = {"+": add, "-": sub, "*": mul, "/": div}
 
     if len(argv) - 1 != 3:
-        print("Unknown operator. Available operators: +, -, * and /")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     if argv[2] not in list(operations.keys()):
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
 
     a = int(argv[1])
