fbe79f4834156d528bda1cf6e90694ded3aa50ba
JezerGonzalez <jezergonzalez@gmail.com>
2024-05-13 12:40:34 -0400

    added task 4

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = my_list[:]
+    for index in range(0, len(my_list)):
+        if idx < 0 or idx > len(my_list):
+            return my_list
+        elif index == idx:
+            new_list[index] = element
+            return new_list
+            
