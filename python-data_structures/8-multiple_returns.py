dd009688fa1b6fa519d74064c66095bbf75bf4ed
JezerGonzalez <jezergonzalez@gmail.com>
2024-05-13 16:06:20 -0400

    ahora si lol

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    if len(sentence):
-        first_letter = sentence[:1]
-    else:
-        None
+    first_letter = sentence[:1] if len(sentence) > 0 else None
     return (length, first_letter)
