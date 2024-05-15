882c5b293c0812eef64fa0100005e9357cfd1685
JezerGonzalez <jezergonzalez@gmail.com>
2024-05-15 12:17:20 -0400

    added task 0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for index in range(0, x):
+            print("{:d}".format(my_list[index]), end="" if index < x - 1 else "\n")
+    except IndexError:
+        print()
+        return index
+    return index + 1
