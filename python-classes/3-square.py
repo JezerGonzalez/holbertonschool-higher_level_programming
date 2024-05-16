045f9642bc289012ba7b66949a55faed708e3fe5
JezerGonzalez <jezergonzalez@gmail.com>
2024-05-16 14:51:14 -0400

    added task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Empty Class that defines a square"""
+
+
+class Square():
+    """Start of the Class"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        return self.__size * self.__size
