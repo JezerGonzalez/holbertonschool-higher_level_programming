9c1313fd51ceddce9664a6d91899e7ff1ff4c9b3
JezerGonzalez <jezergonzalez@gmail.com>
2024-05-16 16:47:48 -0400

    I shouldve been aborted

---
+++ python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Empty Class that defines a square"""
+
+
+class Square():
+    """Start of the Class"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of the square"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Gets the value of size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the value"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """Prints the square using the '#' character"""
+        if self.__size == 0:
+            print()
+            return
+        else:
+            for index in range(self.__size):
+                print("#" * self.__size)
