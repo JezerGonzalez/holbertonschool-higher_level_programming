7b7492c6ec74ec04409711bf75f1828d7191cf20
JezerGonzalez <jezergonzalez@gmail.com>
2024-05-16 15:34:51 -0400

    ya

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -15,7 +15,7 @@ class Square():
     def size(self):
         """Gets the value of size"""
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         """Sets the value"""
