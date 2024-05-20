46b8ba624d2a7a4513ad17b97ad4e2492d8bef5a
JezerGonzalez <jezergonzalez@gmail.com>
2024-05-20 16:02:36 -0600

    added 3

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Documentation"""
+
+def say_my_name(first_name, last_name=""):
+    """Documentation"""
+
+    first = "first_name must be a string"
+    last = "last_name must be a string"
+    
+    if type(first_name) is not str:
+        raise TypeError(first)
+    if type(last_name) is not str:
+        raise TypeError(last)
+    if last_name is "":
+        print(f"My name is {first_name}")
+    else:
+        print(f"My name is {first_name} {last_name}")
