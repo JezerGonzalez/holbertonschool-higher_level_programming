d078dfd4504a7d447ed7a59e6422d96bffd8729a
JezerGonzalez <jezergonzalez@gmail.com>
2024-05-20 12:01:18 -0600

    added task 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    This is a module to add two integers
+"""
+def add_integer(a, b=98):
+    """
+        Takes two integers, validates them and adds them.
+    """
+    error = "unsupported operand type(s) for +: 'NoneType' and 'int'"
+    if a is None or b is None:
+        raise TypeError(error)
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
