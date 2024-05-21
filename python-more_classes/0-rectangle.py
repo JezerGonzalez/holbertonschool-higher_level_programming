e32f697f1f3176fd7c79c4ed185df0e2982c411b
JezerGonzalez <jezergonzalez@gmail.com>
2024-05-21 11:08:53 -0600

    added task 0 and README

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Method that prints out a rectangle"""
+
+
+class Rectangle:
+    """Start of the rectangle class"""
+    def __init__(self):
+        pass
+    
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+Creating a rectangle with classes
