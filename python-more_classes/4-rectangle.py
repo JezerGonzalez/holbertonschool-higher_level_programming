948d03c9ee29db5bb4d1d40090bbcedbae0b2885
JezerGonzalez <jezergonzalez@gmail.com>
2024-05-21 13:23:35 -0600

    pycodestyle

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -67,4 +67,4 @@ class Rectangle:
         return string
 
     def __repr__(self):
-        return(f"Rectangle({self.__width}, {self.__height})")
+        return (f"Rectangle({self.__width}, {self.__height})")
