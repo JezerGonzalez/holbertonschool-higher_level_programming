cd17f3a057e004a7ded095d102a37e63092a6335
JezerGonzalez <jezergonzalez@gmail.com>
2024-05-28 11:44:29 -0600

    idl

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""Appends a string at the end of a text file and returns the number of characters added"""
+"""
+Appends a string at the end of a text file and returns the 
+number of characters added
+"""
 
 
 def append_write(filename="", text=""):
-    """Apends and returns number of characters"""
+    """Writes on file"""
 
     with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
