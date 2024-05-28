c42aae86b997b44af7b3c883c470c22647df074f
JezerGonzalez <jezergonzalez@gmail.com>
2024-05-28 11:35:53 -0600

    added task 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Appends a string at the end of a text file and returns the number of characters added"""
+
+
+def append_write(filename="", text=""):
+    """Apends and returns number of characters"""
+    with open(filename,"w", encoding="utf-8") as file:
+        return file.write(text)
