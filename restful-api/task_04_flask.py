6edc5149df5d2a9c3b0da505e86c09b19426c2ee
JezerGonzalez <jezergonzalez@gmail.com>
2024-05-31 17:49:18 -0400

    fixed everything

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,25 +1,28 @@
-""" Develop a Simple API using Python with Flask"""
 from flask import Flask, jsonify, request
-
 
 app = Flask(__name__)
 
-
+# user data
 users = {}
 
 
-@app.route('/')
+@app.route("/")  # Route for the root URL
 def home():
-    return 'Welcome to the Flask API!'
+    return "Welcome to the Flask API!"
 
 
-@app.route('/status')
+@app.route("/data")  # Route to get list of usernames
+def data():
+    return jsonify(list(users.keys()))
+
+
+@app.route("/status")  # Route to check status
 def status():
-    return 'OK'
+    return "OK"
 
 
-@app.route('/user/<username>')
-def profile(username):
+@app.route("/users/<username>")  # Route to get user details by username
+def find(username):
     user = users.get(username)
     if user:
         return jsonify(user)
@@ -27,23 +30,18 @@ def profile(username):
         return jsonify({"error": "User not found"}), 404
 
 
-@app.route('/add_user', methods=['POST'])
-def add_user():
-    data = request.json
-    username = data.get('username')
+@app.route('/add_user', methods=['POST'])  # Route to add a new user
+def add():
+    newuser = request.json
+    username = newuser.get("username")
 
     if not username:
-        return jsonify({'error': 'Username is required'}), 400
-    if username in users:
-        return jsonify({'error': 'User already exists'}), 400
-    users[username] = data
-    return jsonify({'message': 'User added', 'user': data}), 201
+        return jsonify({"error": "Username is required"}), 400
+    if username == users:
+        return jsonify({"error": "User already exists"}), 400
+    users[username] = newuser
+    return jsonify({"message": "User added", "user": newuser}), 201
 
 
-@app.route('/data')
-def data():
-    return jsonify(list(users.keys()))
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)  # Run the Flask development server
