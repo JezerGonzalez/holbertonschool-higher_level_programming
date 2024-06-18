5360e13cd3d21abace67158d371b393551d1df0e
JezerGonzalez <jezergonzalez@gmail.com>
2024-06-18 15:08:50 -0400

    ahora si

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS in hbtn_0d_usa states(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
     id INT PRIMARY KEY AUTO_GENERATED NOT NULL,
     name VARCHAR(256) NOT NULL
 )
