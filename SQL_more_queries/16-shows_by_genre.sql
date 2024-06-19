6a7930c83f656b6405b2cb1de00a3040d0042325
JezerGonzalez <jezergonzalez@gmail.com>
2024-06-18 23:57:35 -0400

    ya

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,8 @@
 -- Block
-SELECT tv_shows.title
+SELECT tv_shows.title,
+    tv_genres.name
 FROM tv_shows
-    INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-    INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = "Comedy"
-ORDER BY title ASC;
+    LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC,
+    tv_genres.name ASC;
