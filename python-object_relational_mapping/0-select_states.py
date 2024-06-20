aa8430b56011ecf8d7b1aed9cadbe221756e51fc
JezerGonzalez <jezergonzalez@gmail.com>
2024-06-20 16:04:50 -0400

    added task 0 and README

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Lists all states in database"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        connection = MySQLdb.connect(host = 'localhost',
+                                        port = 3306,
+                                        user = sys.argv[1],
+                                        password = sys.argv[2],
+                                        db = sys.argv[3])
+
+        cursor = connection.cursor()
+
+        cursor.execute("Select * FROM states ORDER BY states.id ASC;")
+
+        states = cursor.fetchall()
+        for state in states:
+            print(state)
+
+        cursor.close()
+        connection.close()
+    except MySQLdb.Error as e:
+        print(f"Error connecting to MySQL or execuiting query: {e}")
