78ab96be3c51856e382457948419b024fa559aaa
JezerGonzalez <jezergonzalez@gmail.com>
2024-06-20 16:11:00 -0400

    ya esta

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,11 +6,11 @@ import sys
 
 if __name__ == "__main__":
     try:
-        connection = MySQLdb.connect(host = 'localhost',
-                                        port = 3306,
-                                        user = sys.argv[1],
-                                        password = sys.argv[2],
-                                        db = sys.argv[3])
+        connection = MySQLdb.connect(host='localhost',
+                                     port=3306,
+                                     user=sys.argv[1],
+                                     password=sys.argv[2],
+                                     db=sys.argv[3])
 
         cursor = connection.cursor()
 
