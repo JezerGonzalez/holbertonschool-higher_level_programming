c16d884688330ae0c6f0b07a4f85d3cb34e813d7
JezerGonzalez <jezergonzalez@gmail.com>
2024-06-21 15:00:06 -0400

    added task 6

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""First state model"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Start of class"""
+    __tablename__ = 'states'
+    id = Column("id", Integer, primary_key=True, autoincrement=True, nullable=False)
+    name =  Column("name", String(128), nullable=False)
