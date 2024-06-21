42c68acaae5353c31ccff336f6375e05e4e95317
JezerGonzalez <jezergonzalez@gmail.com>
2024-06-21 15:58:00 -0400

    added tasj 8

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""First state"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Wrong input")
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                    sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    first_state = session.query(State).order_by(State.id).first()
+
+    if first_state is not None:
+        print(f"{first_state.id}: {first_state.name}")
+
+    session.close()
