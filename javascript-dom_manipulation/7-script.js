cdcdceaaa0e4d3746840ef9057234a0e3492de3d
JezerGonzalez <jezergonzalez@gmail.com>
2024-07-29 14:05:33 -0400

    ya

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -1,16 +1,13 @@
-function fetchCharFromAPI() {
-    const api = "https://swapi-api.hbtn.io/api/people/5/?format=json";
-    const character = document.querySelector("#character");
-
-    fetch(api)
-        .then((response) => response.json())
-        .then((data) => {
-            const charName = data.name;
-            character.textContent = charName;
-        })
-        .catch((error) => {
-            console.error("Error: ", error);
-        });
-}
-
-fetchCharFromAPI();
+const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
+const movie_list = document.getElementById('list_movies');
+fetch(url).then((response) => response.json())
+.then((data) => {
+    data.results.forEach(movie => {
+        const item = document.createElement('li');
+        item.textContent = movie.title;
+        movie_list.append(item);
+    });
+})
+.catch((error) => {
+    console.error("Error :", error);
+});
